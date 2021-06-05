fc9119320b5fd68e488b24d94d98fe55bd4f2c27
somiryu <somiryu@gmail.com>
2021-06-05 16:25:11 -0500

    Flipable

--- Hubs/FlipableCard.js
+++ Hubs/FlipableCard.js
@@ -2,11 +2,11 @@
 //Add back-visibility:hidden to nested divs in the faces if necesary
 import React from 'react'
 
-export default ({width = 300, height = 399, front, back}) =>( 
-	<div className="FlipableCard" style={{width, height}}>
+export default ({ width = 300, height = 399, front, back, foreground = "white", background = "black" }) => (
+	<div className="FlipableCard" style={{ width, height }}>
 		<div className="flip-card-inner">
-			<div className="flip-card-front">{front}</div>
-			<div className="flip-card-back">{back}</div>
+			<div className="flip-card-front" style={{ backgroundColor: foreground }}>{front}</div>
+			<div className="flip-card-back" style={{ backgroundColor: background }}>{back}</div>
 		</div>
 	</div>
 )
